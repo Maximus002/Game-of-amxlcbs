d06a998b0d22cd4ba82bdb2e3685031773bf35a7
The Wired <105239446+Maximus002@users.noreply.github.com>
2022-12-15 18:55:24 +0300

    Update Game.cpp
    Game v1.0

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -2,6 +2,11 @@
 #include <cstdlib> // для exit()
 #include <windows.h>
 #include <conio.h>
+#include <mmsystem.h>
+//переменные 
+int x;
+int y;
+
 
 
 using namespace std;
@@ -14,8 +19,8 @@ int prompt_menu_item()
     int variant;
     cout << "Выберите что нибудь\n" << endl;
     cout << "1. Тест обработки нажатий\n"
-        << "2. Звук\n"
-        << "3. Выход\n" << endl;
+        << "2. Тест мува\n"
+        << "3. Тест рандомайзера\n" << endl;
     cout << ">>> ";
     cin >> variant;
     return variant;
@@ -55,9 +60,50 @@ int main(int argc, char* argv[])
         }
         break;
 
-    
+    case 2:
+        int move(); {
+            while (true) {
+                if (_kbhit()) {
+                    switch (_getch()) {
+                    case 72: //вверх
+                        y = y+1;
+                        cout << y << " " << "<--- По оси y" << " " << x << " " << "<--- По оси x" << endl;
+                        break;
+                    case 80: //вниз
+                        y = y-1;
+                        cout << y << " " << "<--- По оси y" << " " << x << " " << "<--- По оси x" << endl;
+                        break;
+                    case 75: //влево 
+                        x = x-1;
+                        cout << y << " " << "<--- По оси y" << " " << x << " " << "<--- По оси x" << endl;
+                        break;
+                    case 77: //вправо
+                        x = x+1;
+                        cout << y << " " << "<--- По оси y" << " " << x << " " << "<--- По оси x" << endl;
+                        break;
+                    
+                    }
+                }
+            }
 
+        }
     case 3:
+        int randomaizer(int argc, char* argv[]);
+        {
+            
+            cout << "RAND_MAX = " << RAND_MAX << endl; // константа, хранящая максимальный предел из интервала случайных чисел
+            cout << "Ваше случайное число = " << rand() % 100 << endl;
+            cout << "Ваше случайное число = " << rand() % 20 << endl;
+            cout << "Ваше случайное число = " << rand() % 12 << endl;
+            cout << "Ваше случайное число = " << rand() % 8 << endl;
+            cout << "Ваше случайное число = " << rand() % 6 << endl;// запуск генератора случайных чисел
+            system("pause");
+            return 0;
+        }
+
+
+
+    case 4:
         cout << "..." << endl;
         exit(EXIT_SUCCESS);
         break;
